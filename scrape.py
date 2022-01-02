2b7fcfa69bc64c855a44d2d43587251b209b464b
Zen Lee <iarezenly@gmail.com>
2022-01-02 21:33:55 +0800

    Add multiple page scraping

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import os
-import subprocess
+import time
 
 class NyaaScraper:
     SUBBED = '2'
@@ -28,8 +28,19 @@ class NyaaScraper:
         f.close()
 
     def scrape(self, search, isSubbed):
-        url = self.constructURL(search, isSubbed)
+        url = self.constructURL(search, isSubbed, 1)
         rawHtml = requests.get(url).text
+        soup = BeautifulSoup(rawHtml, 'lxml')
+        buttonList = soup.find('ul', class_ = 'pagination')
+        numPage = self.extractNumPage(buttonList)
+        self.scrapeHelper(rawHtml)
+        for i in range(2, int(numPage)+1):
+            url = self.constructURL(search, isSubbed, i)
+            time.sleep(self.reqDelay)
+            rawHtml = requests.get(url).text
+            self.scrapeHelper(rawHtml)
+        
+    def scrapeHelper(self, rawHtml):
         soup = BeautifulSoup(rawHtml, 'lxml')
         entries = soup.findAll('tr', class_ = 'success')
         for entry in entries:
@@ -37,13 +48,19 @@ class NyaaScraper:
             # remove commas for csv formatting
             data = list(map(lambda x : x.replace(',', ''), data))
             self.writeToFile(','.join(data) + '\n')
-        
 
-    def constructURL(self, search, isSubbed):
-        url = f'{self.source}/?f=2&c=1_$SUB&q=$TITLE'
+    def constructURL(self, search, isSubbed, pageNum):
+        url = f'{self.source}/?f=2&c=1_$SUB&q=$TITLE&p=$PAGENUM'
         url = url.replace('$TITLE', search)
         url = url.replace('$SUB', self.SUBBED if isSubbed else self.RAW)
+        url = url.replace('$PAGENUM', str(pageNum))
         return url
+
+    def extractNumPage(self, buttonList):
+        buttons = buttonList.findAll('li')
+        # second last button is last page, last button is next button
+        pageButton = buttons[len(buttons)-2]
+        return pageButton.find('a').text
 
     def extractData(self, entry):
         columns = entry.findAll('td')
