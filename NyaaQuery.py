553c206febebd04a8b41f8db72bb95ad8e68da01
Zen Lee <iarezenly@gmail.com>
2022-01-03 18:24:02 +0800

    Add sorting by dl,size,seeders,leechers

--- NyaaQuery.py
+++ NyaaQuery.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 class NyaaQuery:
     def __init__(self, path):
@@ -26,10 +27,35 @@ class NyaaQuery:
     def filterByUploader(self, uploader):
         return list(filter(lambda entry : True if self.getUploader(entry).lower() == uploader.lower() else False, self.data))
 
+    def sortByDownloads(self, isDsc):
+        return sorted(self.data, key = lambda x : int(x.split(',')[7]), reverse=isDsc)
+
+    def sortByLeechers(self, isDsc):
+        return sorted(self.data, key = lambda x : int(x.split(',')[6]), reverse=isDsc)
+
+    def sortBySeeders(self, isDsc):
+        return sorted(self.data, key = lambda x : int(x.split(',')[5]), reverse=isDsc)
+
+    def sortBySize(self, isDsc):
+        def comparator(size):
+            split = size.split(' ')
+            val = float(split[0])
+            unit = split[1]
+            power = 1
+            if unit == 'KiB':
+                power = 3
+            elif unit == 'MiB':
+                power = 6
+            elif unit == 'GiB':
+                power = 9
+            return val * math.pow(10, power)
+        return sorted(self.data, key = lambda x : comparator(x.split(',')[3]), reverse=isDsc)
+
     def getUploader(self, entry):
         name = entry.split(',')[0]
         uploader = name.split(' ')[0]
         uploader = uploader[1 : len(uploader)-1]
         return uploader
 
-print(NyaaQuery('db.csv').getUploaders())
+for i in NyaaQuery('db.csv').sortBySeeders(True):
+    print(i)
